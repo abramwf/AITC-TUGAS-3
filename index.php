02889da9e3f72a2f0c269bdc93454a9d1876cde7
abramwf <91315242+abramwf@users.noreply.github.com>
2022-08-18 23:34:31 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+define('PROJECT_ROOT', __DIR__);
+
+$route = strtok($_SERVER["REQUEST_URI"], '?');
+
+require_once PROJECT_ROOT.'/controllers/MahasiswaController.php';
+
+switch($route){
+    case '/':
+        $mahasiswa = new MahasiswaController;
+        $mahasiswa->index();
+        break;
+    case '/create':
+        $mahasiswa = new MahasiswaController;
+        $mahasiswa->create();
+        break;
+    case '/store':
+        $mahasiswa = new MahasiswaController;
+        $mahasiswa->store();
+        break;
+    case '/edit':
+        $mahasiswa = new MahasiswaController;
+        $mahasiswa->edit();
+        break;
+    case '/delete': 
+        $mahasiswa = new MahasiswaController;
+        $mahasiswa->delete();
+    default:
+        echo "Halaman Tidak Ditemukan";
+        break;
+}
+
+?>
